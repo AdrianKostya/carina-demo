b473c225b4468009a156b7955b6396ac998c763b
Adrian Kostya <akostya@solvd.com>
2021-04-30 14:40:03 +0300

    added verifyArticleName, verifySearchingProces, verifyGlossParagraph

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/components/PhoneFinder.java
@@ -0,0 +1,13 @@
+package com.qaprosoft.carina.demo.gui.components;
+
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+
+public class PhoneFinder extends AbstractUIObject {
+
+    public PhoneFinder  (WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+}
--- src/test/java/com/qaprosoft/carina/demo/GSMArenaTest.java
+++ src/test/java/com/qaprosoft/carina/demo/GSMArenaTest.java
@@ -1,21 +1,18 @@
 package com.qaprosoft.carina.demo;
 
+import java.util.HashMap;
 import java.util.List;
 
+import com.qaprosoft.carina.core.foundation.dataprovider.annotations.XlsDataSourceParameters;
+import com.qaprosoft.carina.demo.gui.components.*;
+import com.qaprosoft.carina.demo.gui.pages.*;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import com.qaprosoft.carina.demo.gui.pages.ArticlePage;
-import com.qaprosoft.carina.demo.gui.pages.HomePage;
-import com.qaprosoft.carina.demo.gui.pages.LoginPage;
-import com.qaprosoft.carina.demo.gui.pages.NewsPage;
-import com.qaprosoft.carina.demo.gui.components.NewsItem;
 import com.qaprosoft.carina.core.foundation.AbstractTest;
-import com.qaprosoft.carina.demo.gui.components.HeaderGSM;
-import com.qaprosoft.carina.demo.gui.components.LoginForm;
-import com.qaprosoft.carina.demo.gui.components.UserGSM;
 import com.qaprosoft.carina.demo.gui.services.LoginService;
 import com.qaprosoft.carina.demo.gui.services.UserService;
 
@@ -77,8 +74,10 @@ public class GSMArenaTest extends AbstractTest {
         Assert.assertEquals(newsItemTitle, articlePage.getArticleTitle(), "Titles is not the same");
     }
 
-    @Test
-    public void verifySearchingProcess(){
+    @Test(dataProvider = "dataProvider")
+    @XlsDataSourceParameters(path = "xls/phone.xlsx", sheet = "phoneSheet", dsUid = "TUID")
+    public void verifySearchingProcessWithXLS(HashMap<String, String> args){
+        final String itemName = args.get("brand");
         LoginService loginService = new LoginService(getDriver());
         UserService userService = new UserService();
         UserGSM userGSM = userService.getUser();
@@ -86,12 +85,65 @@ public class GSMArenaTest extends AbstractTest {
         Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
         NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
         Assert.assertTrue(newsPage.isPageOpened());
-        String itemName = "iphone";
         List<NewsItem> newsItems= newsPage.searchNews(itemName);
         for (NewsItem news:newsItems){
             System.out.println("String NEWS: "+news.readTitle());
             Assert.assertTrue(StringUtils.containsIgnoreCase(news.readTitle(),  itemName), "titles not the same");
         }
+    }
+
+    @Test
+    @Parameters({ "phoneSearch" })
+    public void verifySearchingProcess(String search){
+        LoginService loginService = new LoginService(getDriver());
+        UserService userService = new UserService();
+        UserGSM userGSM = userService.getUser();
+        HomePage homePage= loginService.login(userGSM.getEmail(), userGSM.getPassword());
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
+        Assert.assertTrue(newsPage.isPageOpened());
+        List<NewsItem> newsItems= newsPage.searchNews(search);
+        for (NewsItem news:newsItems){
+            System.out.println("String NEWS: "+news.readTitle());
+            Assert.assertTrue(StringUtils.containsIgnoreCase(news.readTitle(),  search), "titles not the same");
+        }
+    }
+
+    @Test(description = "JIRA#AUTO-0008")
+    public void testVerifyGlossaryFirstLetterInTitle() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        GlossaryPage glossaryPage = homePage.getFooterMenu().openGlossaryPage();
+        Assert.assertTrue(glossaryPage.isPageOpened(), "Glossary Page isn't opened");
+        Assert.assertTrue(glossaryPage.verifyHeaderAndTextEquality(), "Elements don't match");
+        Assert.assertTrue(glossaryPage.verifyTitleByFirstLetter(), "Not alphabet sort");
+    }
+
+    @Test(description = "JIRA#AUTO-0008")
+    public void testVerifyGlossaryTextByAlphabets() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        GlossaryPage glossaryPage = homePage.getFooterMenu().openGlossaryPage();
+        Assert.assertTrue(glossaryPage.isPageOpened(), "Glossary Page isn't opened");
+        Assert.assertTrue(glossaryPage.verifyHeaderAndTextEquality(), "Header and text items don't match");
+        Assert.assertTrue(glossaryPage.verifyAlphabeticalOrder(glossaryPage.glossaryItem));
+    }
+
+    @Test
+    public void verifyHamburgerPageOpening(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        HamburgerMenu hamburgerMenu = homePage.getHeaderGSM().getHamburgerMenu();
+        Assert.assertTrue(hamburgerMenu.openHomePage().isPageOpened(), "HomePage is not opened");
+        Assert.assertTrue(hamburgerMenu.openNewsPage().isPageOpened(), "NewsPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openReviewsPage().isPageOpened(), "ReviewsPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openVideosPage().isPageOpened(), "Videos is not opened");
+        Assert.assertTrue(hamburgerMenu.openFeaturedPage().isPageOpened(), "FeaturedPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openPhoneFinderPage().isPageOpened(), "PhoneFinderPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openDealsPage().isPageOpened(), "DealsPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openToolsPage().isPageOpened(), "ToolsPage is not opened");
+        Assert.assertTrue(hamburgerMenu.openCoveragePage().isPageOpened(), "CoveragePage is not opened");
+        Assert.assertTrue(hamburgerMenu.openContactPage().isPageOpened(), "ContactPage is not opened");
     }
 
 }
