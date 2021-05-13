4766a49c4b5102c7141ee87b35027b5c4eca0b12
Adrian Kostya <akostya@solvd.com>
2021-05-13 15:07:30 +0300

    Api parse int and send ID in URL

--- src/main/java/com/qaprosoft/carina/demo/api/gorest/DeleteUser.java
+++ src/main/java/com/qaprosoft/carina/demo/api/gorest/DeleteUser.java
@@ -5,9 +5,11 @@ import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
 public class DeleteUser extends AbstractApiMethodV2 {
 
-    public  DeleteUser(){
+    public  DeleteUser(int id){
         super(null, "api.gorest/_get/_delete/rs.json");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
+        replaceUrlPlaceholder("id", String.valueOf(id));
         request.header("Authorization", "Bearer 7cc2706fcfa7044adc1a9ec1632ef9c7be7001181f0aa222147e7dd7c046f5f7");
+
     }
 }
--- src/test/java/com/qaprosoft/carina/demo/MyApiTest.java
+++ src/test/java/com/qaprosoft/carina/demo/MyApiTest.java
@@ -5,9 +5,11 @@ import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
 import com.qaprosoft.carina.demo.api.gorest.DeleteUser;
 import com.qaprosoft.carina.demo.api.gorest.GetUser;
 import com.qaprosoft.carina.demo.api.gorest.PostUser;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 public class MyApiTest extends AbstractTest {
+    static int id=0;
     @Test
     public void getUser(){
         GetUser getUser = new GetUser();
@@ -17,16 +19,23 @@ public class MyApiTest extends AbstractTest {
     }
 
     @Test
-    public void postUser(){
+    public void postAndDeleteUser(){
         PostUser postUser = new PostUser();
         postUser.expectResponseStatus(HttpResponseStatusType.OK_200);
-        postUser.callAPI();
+        String rs = postUser.callAPI().asString();
         postUser.validateResponse();
+        System.out.println("Recieve is :"+rs);
+         id = Integer.parseInt(new  JsonPath(rs).getString("data.id"));
+        System.out.println("ID is : "+id);
+        /*DeleteUser deleteUser = new DeleteUser(id);
+        deleteUser.expectResponseStatus(HttpResponseStatusType.OK_200);
+        deleteUser.callAPI();
+        deleteUser.validateResponse();*/
     }
 
     @Test
     public void deleteUser(){
-        DeleteUser deleteUser = new DeleteUser();
+        DeleteUser deleteUser = new DeleteUser(id);
         deleteUser.expectResponseStatus(HttpResponseStatusType.OK_200);
         deleteUser.callAPI();
         deleteUser.validateResponse();
