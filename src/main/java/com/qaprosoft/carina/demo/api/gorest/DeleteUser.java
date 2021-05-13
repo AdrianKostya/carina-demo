26f02edb5c46b433e2c03557b4b511de606ec906
Adrian Kostya <akostya@solvd.com>
2021-05-13 11:01:19 +0300

    Added 2sd clases

--- src/main/java/com/qaprosoft/carina/demo/api/gorest/DeleteUser.java
+++ src/main/java/com/qaprosoft/carina/demo/api/gorest/DeleteUser.java
@@ -1,2 +1,16 @@
-package com.qaprosoft.carina.demo.api.gorest;public class DeleteUser {
+package com.qaprosoft.carina.demo.api.gorest;
+
+import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.utils.Configuration;
+
+public class DeleteUser extends AbstractApiMethodV2 {
+
+    public  DeleteUser(){
+        super(null, "api.gorest/_get/_delete/rs.json");
+        replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
+
+
+
+        request.header("Authorization", "Bearer 7cc2706fcfa7044adc1a9ec1632ef9c7be7001181f0aa222147e7dd7c046f5f7");
+    }
 }
--- src/main/java/com/qaprosoft/carina/demo/api/gorest/PostUser.java
+++ src/main/java/com/qaprosoft/carina/demo/api/gorest/PostUser.java
@@ -5,6 +5,7 @@ import com.qaprosoft.carina.core.foundation.utils.Configuration;
 
 public class PostUser extends AbstractApiMethodV2 {
     public PostUser (){
+
         super("api.gorest/_get/_post/rq.json", "api.gorest/_get/_post/rs.json", "api.gorest/_get/user.properties");
         replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
         request.header("Authorization", "Bearer 7cc2706fcfa7044adc1a9ec1632ef9c7be7001181f0aa222147e7dd7c046f5f7");
